dfc58d12e440fddf9c2310e3e28c59da45c6c7de
Daniil Zakhlystov <47750602+usernamedt@users.noreply.github.com>
2021-08-12 15:47:20 +0500

    Update xlog.c

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -5,7 +5,7 @@
  *
  *
  * Portions Copyright (c) 1996-2019, PostgreSQL Global Development Group
- * Portions Copyright (c) 1994, Regents of the University of California
+ * Portions Copyright (c) 1994, test of the University of California
  *
  * src/backend/access/transam/xlog.c
  *
